2068f9d3d594719168caf58d42872cbfdc79cb45
Çağdaş Aydemir <ac.cagdasaydemir@gmail.com>
2022-06-29 20:47:49 +0300

    fixed validation and integrated generator

--- dinamik-yatırım/dinamik-yatırım_juniorCase/app.js
+++ dinamik-yatırım/dinamik-yatırım_juniorCase/app.js
@@ -1,21 +1,30 @@
+// const tBodyEl = document.getElementsByTagName("tbody")[0];
+
 function getUserInput() {
   const errorMessage = document.getElementById("error-message");
   errorMessage.innerHTML = "";
   let userNumber = document.getElementById("user-number").value;
-  console.log(textGenerator(formValidation(userNumber, errorMessage)))
+  formValidation(userNumber, errorMessage);
   document.getElementById("user-number").value = "";
 }
 
 formValidation = (userInput, messageEl) => {
- const max = 50;
- const  min = 0;
+  const max = 50;
+  const min = 1;
   try {
-    if (userInput == "") throw "empty";
-    if (isNaN(userInput)) throw "not a number";
-    userInput = Number(userInput);
-    if (userInput < min) throw "too low";
-    if (userInput > max) throw "too high";
-    else return userInput;
+    if (userInput == "") {
+      throw "empty";
+    } else if (isNaN(userInput)) {
+      throw "not a number";
+    } else if (userInput < min) {
+      userInput = Number(userInput);
+      throw "too low";
+    } else if (userInput > max) {
+      userInput = Number(userInput);
+      throw "too high";
+    } else {
+      console.log(textGenerator(userInput));
+    }
   } catch (err) {
     messageEl.innerHTML = "Input is " + err;
   }
@@ -36,4 +45,13 @@ textGenerator = (length) => {
   return resultArr;
 };
 
+// addItemToHTML = () => {
+//   let addedItem = ` <tr>
+//   <th scope="row">1</th>
+//   <td>Mark</td>
+//   <td>Otto</td>
+// </tr>`;
+// tBodyEl.insertAdjacentHTML("beforeend", addedItem);
+// };
 
+// addItemToHTML();

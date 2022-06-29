a1399bef072ce37cde5a67a5e08d95881e7e28ef
Çağdaş Aydemir <ac.cagdasaydemir@gmail.com>
2022-06-29 18:21:24 +0300

    textGenerator and formValidation

--- dinamik-yatırım/dinamik-yatırım_juniorCase/README.md
+++ dinamik-yatırım/dinamik-yatırım_juniorCase/README.md
@@ -4,13 +4,15 @@ Dinamik Yatırım Recruitment Case - JUNIOR
 
 ## Question
 
-                            JUNIOR SOFTWARE DEVELOPER CASE
+JUNIOR SOFTWARE DEVELOPER CASE
 
 <!-- 1 Adım: Adaydan bir web sayfası yapması istenmektedir. Sayfanın üst tarafında bir Input bir tane de
 'Gönder' butonu olacak. -->
-Kullanıcı Input’a 1-50 arası bir rakam girebilecek. Kullanıcı bir rakam girip Gönder butonuna
+
+<!-- Kullanıcı Input’a 1-50 arası bir rakam girebilecek. Kullanıcı bir rakam girip Gönder butonuna -->
 tıkladığında girdiği rakam uzunluğunda 100 tane rastgele metin yaratılıp veritabanına kaydedilecek.
 Rastgele yaratılacak metinler sadece Türkçe'deki küçük harflerden oluşabilecek.
+
 2. Adım: Oluşturulan web sayfasının alt tarafına bir tablo yapılacak. Tablonun üstünde bulunan 'Getir'
 butonuna tıklandığında daha önce veritabanına kaydedilen metinler bu tabloda listelenecek. Ayrıca
 tabloda sıralama özelliği de bulunacak.
@@ -19,4 +21,18 @@ Not: Error Handling önemlidir
 
 ## Solution - Algorithm
 
-- 
+<!-- - form 
+    - form reset onClick
+    - formValidation and errorMessage
+    - get userInput -->
+
+- generator
+    <!-- - length.userInput x 100 times
+    - türkçe karakterler lowerCase -->
+    - database ? cache ?
+
+- dataTable 
+    - getir button
+    - use localStorage
+    - sort 
+
--- dinamik-yatırım/dinamik-yatırım_juniorCase/app.js
+++ dinamik-yatırım/dinamik-yatırım_juniorCase/app.js
@@ -1,17 +1,37 @@
+function getUserInput() {
+  const errorMessage = document.getElementById("error-message");
+  errorMessage.innerHTML = "";
+  let userNumber = document.getElementById("user-number").value;
+ console.log(textGenerator(formValidation(userNumber, errorMessage)))
+  document.getElementById("user-number").value = "";
+}
 
-function formValidation() {
-
-  
-    let userNumber = document.getElementById("user-number").value;
-    // If x is Not a Number or less than one or greater than 10
-    let text;
-    if (isNaN(userNumber) || userNumber < 1 || userNumber > 51) {
-      text = "Input not valid";
-    } else {
-      text = "Input OK";
-      document.getElementById("user-number").value = "";
-    }
-    document.getElementById("error-message").innerHTML = text;
-
-    console.log(userNumber)
+formValidation = (userInput, messageEl) => {
+  try {
+    if (userInput == "") throw "empty";
+    if (isNaN(userInput)) throw "not a number";
+    userInput = Number(userInput);
+    if (userInput < 0) throw "too low";
+    if (userInput > 51) throw "too high";
+    else return userInput;
+  } catch (err) {
+    messageEl.innerHTML = "Input is " + err;
   }
+};
+
+textGenerator = (length) => {
+  var quantity = 100;
+  var resultArr = [];
+  var characters = "abcçdefgğhıijklmnoöprsştuüvyz";
+  var charactersLength = characters.length;
+  for (let j = 0; j < quantity; j++) {
+    var result = "";
+    for (var i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    resultArr[j] = result;
+  }
+  return resultArr;
+};
+
+

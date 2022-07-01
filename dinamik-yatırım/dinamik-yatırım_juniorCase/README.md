66e481d927d9b2ccac09a4e56f733480c8f19619
Çağdaş Aydemir <ac.cagdasaydemir@gmail.com>
2022-07-01 16:44:48 +0300

    changed table, added sort feature

--- dinamik-yatırım/dinamik-yatırım_juniorCase/README.md
+++ dinamik-yatırım/dinamik-yatırım_juniorCase/README.md
@@ -6,36 +6,36 @@ Dinamik Yatırım Recruitment Case - JUNIOR
 
 JUNIOR SOFTWARE DEVELOPER CASE
 
-<!-- 1 Adım: Adaydan bir web sayfası yapması istenmektedir. Sayfanın üst tarafında bir Input bir tane de
-'Gönder' butonu olacak. -->
+1 Adım: Adaydan bir web sayfası yapması istenmektedir. Sayfanın üst tarafında bir Input bir tane de
+'Gönder' butonu olacak.
 
-<!-- Kullanıcı Input’a 1-50 arası bir rakam girebilecek. Kullanıcı bir rakam girip Gönder butonuna -->
-<!-- tıkladığında girdiği rakam uzunluğunda 100 tane rastgele metin yaratılıp veritabanına kaydedilecek.
-Rastgele yaratılacak metinler sadece Türkçe'deki küçük harflerden oluşabilecek. -->
+Kullanıcı Input’a 1-50 arası bir rakam girebilecek. Kullanıcı bir rakam girip Gönder butonuna
+tıkladığında girdiği rakam uzunluğunda 100 tane rastgele metin yaratılıp veritabanına kaydedilecek.
+Rastgele yaratılacak metinler sadece Türkçe'deki küçük harflerden oluşabilecek.
 
-<!-- 2. Adım: Oluşturulan web sayfasının alt tarafına bir tablo yapılacak. Tablonun üstünde bulunan 'Getir' -->
+2. Adım: Oluşturulan web sayfasının alt tarafına bir tablo yapılacak. Tablonun üstünde bulunan 'Getir'
 butonuna tıklandığında daha önce veritabanına kaydedilen metinler bu tabloda listelenecek. Ayrıca
 tabloda sıralama özelliği de bulunacak.
-<!-- Not: Error Handling önemlidir -->
+Not: Error Handling önemlidir
 
 
 ## Solution - Algorithm
 
-<!-- - form 
+- form 
     - form reset onClick
     - formValidation and errorMessage
-    - get userInput -->
+    - get userInput
 
-<!-- - generator
+- generator
     - length.userInput x 100 times
-    - türkçe karakterler lowerCase -->
+    - türkçe karakterler lowerCase
     
 
 - dataTable 
-    <!-- - getir button -->
-    <!-- - add items to list -->
-    <!-- - add items to html -->
-    <!-- - use localStorage -->
+    - getir button
+    - add items to list
+    - add items to html
+    - use localStorage
     - sort 
-    
+  
 
--- dinamik-yatırım/dinamik-yatırım_juniorCase/app.js
+++ dinamik-yatırım/dinamik-yatırım_juniorCase/app.js
@@ -1,87 +1,73 @@
 let words = [];
-const wordItemsLS = localStorage.getItem("wordItemsLS");
-if (wordItemsLS) {
-  words = JSON.parse(wordItemsLS);
-}
-
 let Word = {
-  id: 0,
-  text: "",
-  length: 0,
+    id: 0,
+    text: "",
+    length: 0,
 };
 
+const wordItemsLS = localStorage.getItem("wordItemsLS");
 const tBodyEl = document.getElementsByTagName("tbody")[0];
+const alertIcon = `<i class="align-self-center mx-1 fa-solid fa-triangle-exclamation"></i>`;
 
-function getUserInput() {
-  const errorMessage = document.getElementById("error-message");
-  errorMessage.innerHTML = "";
-  let userNumber = document.getElementById("user-number").value;
-  formValidation(userNumber, errorMessage);
+if (wordItemsLS) {
+    words = JSON.parse(wordItemsLS);
 }
 
 const idGenerator = () => {
-  return Math.floor(Math.random() * 100000 + 1);
+    return Math.floor(Math.random() * 100000 + 1);
+};
+
+const getUserInput = () => {
+    const errorMessage = document.getElementById("error-message");
+    errorMessage.innerHTML = "";
+    let userNumber = document.getElementById("user-number").value;
+    formValidation(userNumber, errorMessage);
 };
 
 formValidation = (userInput, messageEl) => {
-  const max = 50;
-  const min = 1;
-  try {
-    if (userInput == "") {
-      throw "empty";
-    } else if (isNaN(userInput)) {
-      throw "not a number";
-    } else if (userInput < min) {
-      userInput = Number(userInput);
-      throw "too low";
-    } else if (userInput > max) {
-      userInput = Number(userInput);
-      throw "too high";
-    } else {
-      textGenerator(userInput, 100);
-      document.getElementById("user-number").value = "";
+    const max = 50;
+    const min = 1;
+    try {
+        if (userInput == "") {
+            throw "empty";
+        } else if (isNaN(userInput)) {
+            throw "not a number";
+        } else if (userInput < min) {
+            userInput = Number(userInput);
+            throw "too low";
+        } else if (userInput > max) {
+            userInput = Number(userInput);
+            throw "too high";
+        } else {
+            textGenerator(userInput, 100);
+            document.getElementById("user-number").value = "";
+        }
+    } catch (err) {
+        messageEl.innerHTML = "Input is " + err;
+        messageEl.insertAdjacentHTML("afterbegin", alertIcon);
     }
-  } catch (err) {
-    messageEl.innerHTML = "Input is " + err;
-  }
 };
 
 textGenerator = (textLength, quantity) => {
-  var characters = "abcçdefgğhıijklmnoöprsştuüvyz";
-  var charactersLength = characters.length;
-  var result = "";
-  for (let j = 0; j < quantity; j++) {
+    var characters = "abcçdefgğhıijklmnoöprsştuüvyz";
+    var charactersLength = characters.length;
     var result = "";
-    for (var i = 0; i < textLength; i++) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    for (let j = 0; j < quantity; j++) {
+        var result = "";
+        for (var i = 0; i < textLength; i++) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        wordItem = {
+            ...Word,
+            id: idGenerator(),
+            text: result,
+            length: textLength,
+        };
+        words = [...words, wordItem];
     }
-    wordItem = {
-      ...Word,
-      id: idGenerator(),
-      text: result,
-      length: textLength,
-    };
-    words = [...words, wordItem];
-  }
-  saveWordsToLS();
+    saveWordsToLS();
 };
 
 const saveWordsToLS = () => {
-  localStorage.setItem("wordItemsLS", JSON.stringify(words));
+    localStorage.setItem("wordItemsLS", JSON.stringify(words));
 };
-
-const insertWordItemsToList = (wordList) => {
-  for (let i = 0; i < wordList.length; i++) {
-    let addedItem = ` <tr>
-    <th scope="row">${wordList[i].id}</th>
-    <td>${wordList[i].text}</td>
-    <td>${wordList[i].length}</td>
-  </tr>`;
-
-    tBodyEl.insertAdjacentHTML("afterbegin", addedItem);
-  }
-};
-
-document.getElementById("getir-btn").addEventListener("click", function () {
-  insertWordItemsToList(words);
-});

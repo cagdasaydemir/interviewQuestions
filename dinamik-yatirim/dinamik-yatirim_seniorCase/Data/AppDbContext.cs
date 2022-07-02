5fbd26759ca3e8a53f19cc94089497a27ffd7d94
Çağdaş Aydemir <ac.cagdasaydemir@gmail.com>
2022-07-02 20:24:46 +0300

    set and check DB

---
+++ dinamik-yatirim/dinamik-yatirim_seniorCase/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using dinamik_yatirim_seniorCase.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dinamik_yatirim_seniorCase.Data
+{
+    public class AppDbContext :DbContext 
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Word> Words { get; set; }
+    }
+}
--- dinamik-yatirim/dinamik-yatirim_seniorCase/Models/Word.cs
+++ dinamik-yatirim/dinamik-yatirim_seniorCase/Models/Word.cs
@@ -1,6 +1,16 @@
-﻿namespace dinamik_yatirim_seniorCase.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dinamik_yatirim_seniorCase.Models
 {
     public class Word
     {
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Range(1, 50, ErrorMessage = "Lütfen 1-50 arasında bir sayı giriniz !")]
+        public int Length { get; set; }
+        public string Text { get; set; }
+        //public DateTime CreatedTime { get; set; } = DateTime.Now;
     }
 }

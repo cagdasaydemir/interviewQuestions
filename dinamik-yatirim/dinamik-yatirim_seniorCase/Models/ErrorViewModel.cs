1fbd6cde236fd2534bf0ebf03fe611f3125c80f6
Çağdaş Aydemir <ac.cagdasaydemir@gmail.com>
2022-07-02 15:43:35 +0300

    initialize MvcProject

---
+++ dinamik-yatirim/dinamik-yatirim_seniorCase/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dinamik_yatirim_seniorCase.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

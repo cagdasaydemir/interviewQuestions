19abfc03112c7700de741153681191aa91bd0f51
Çağdaş Aydemir <ac.cagdasaydemir@gmail.com>
2022-07-03 22:24:17 +0300

    created and finished

---
+++ dinamik-yatirim/dinamik-yatirim_seniorCase-WinFormApp/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace dinamik_yatirim_seniorCase_WinFormApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStatus = new System.Windows.Forms.Button();
+            this.timer3Secs = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(185, 85);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lütfen bir Rakam Giriniz :";
+            // 
+            // textBox
+            // 
+            this.textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.textBox.Location = new System.Drawing.Point(450, 78);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(54, 35);
+            this.textBox.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(190, 131);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(146, 42);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(358, 131);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(146, 42);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStatus
+            // 
+            this.btnStatus.Location = new System.Drawing.Point(190, 189);
+            this.btnStatus.Name = "btnStatus";
+            this.btnStatus.Size = new System.Drawing.Size(314, 52);
+            this.btnStatus.TabIndex = 5;
+            this.btnStatus.Text = "Connection Status";
+            this.btnStatus.UseVisualStyleBackColor = true;
+            this.btnStatus.Click += new System.EventHandler(this.btnStatus_Click);
+            // 
+            // timer3Secs
+            // 
+            //this.timer3Secs.Tick += new System.EventHandler(this.timer3Secs_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(786, 347);
+            this.Controls.Add(this.btnStatus);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStatus;
+        private System.Windows.Forms.Timer timer3Secs;
+    }
+}
+
---
+++ dinamik-yatirim/dinamik-yatirim_seniorCase-WinFormApp/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dinamik_yatirim_seniorCase_WinFormApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection conn = new SqlConnection("Data Source=MSI;Initial Catalog=WordsDB;Integrated Security=True");
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            validateInput();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (conn.State.ToString() == "Open")
+            {
+                timer3Secs.Stop();
+                conn.Close();
+                MessageBox.Show("Bağlantı Kapatıldı !");
+            }
+            else
+            {
+                MessageBox.Show("Bağlantı ZATEN Kapalı !");
+            }
+        }
+
+        private void btnStatus_Click(object sender, EventArgs e)
+        {
+            if (conn.State.ToString() == "Open")
+            {
+                MessageBox.Show("Bağlantı Açık !");
+            }
+            else
+            {
+                MessageBox.Show("Bağlantı Kapalı !");
+            }
+        }
+        private string textGenerator(int textLength)
+        {
+            Random random = new Random();
+            var result = new StringBuilder();
+            const string charS = "abcçdefgğhıijklmnoöprsştuüvyz";
+            const string firstChar = "ABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZ";
+            char c;
+            for (var i = 0; i < textLength; i++)
+            {
+                if (i == 0)
+                {
+                    c = firstChar[random.Next(0, firstChar.Length)];
+                }
+                else
+                {
+                    c = charS[random.Next(0, charS.Length)];
+                }
+                result.Append(c);
+            }
+            return result.ToString();
+        }
+        private void validateInput()
+        {
+            var userInput = textBox.Text;
+            int distance;
+            try
+            {
+                if (userInput.Length == 0)
+                {
+                    throw new Exception("Girdi boş, Lütfen bir Rakam giriniz !");
+                }
+                else if (!(int.TryParse(userInput, out distance)))
+                {
+                    throw new Exception("Lütfen Metin yerine Rakam giriniz !");
+                }
+                else
+                {
+                    int userInputInt = Convert.ToInt32(userInput);
+                    if (userInputInt <= 0)
+                    {
+                        throw new Exception("Sayı 1'den Küçük olamaz !");
+                    }
+                    else if (userInputInt > 9)
+                    {
+                        throw new Exception("Rakam 9'dan Büyük olamaz !");
+                    }
+                    else
+                    {
+                        if (conn.State.ToString() == "Closed")
+                        {
+                            conn.Open();
+                            timer3Secs = new Timer();
+                            timer3Secs.Tick += new EventHandler(timer3Secs_Tick);
+                            timer3Secs.Interval = 3000; // in miliseconds
+                            timer3Secs.Start();
+                            MessageBox.Show("Başarılı bir Şekilde Veritabanına Bağlanıldı, ve Veri aktarılmaya başlandı !!");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Bağlantı ZATEN Açık !");
+                        }
+
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+        private void timer3Secs_Tick(object sender, EventArgs e)
+        {
+            addToDb();
+        }
+        private void addToDb()
+        {
+            var userInput = textBox.Text;
+            int userInputInt = Convert.ToInt32(userInput);
+
+            SqlCommand cmd = new SqlCommand("INSERT INTO Words (Length,Text) VALUES (@WordLength,@WordText)", conn);
+            cmd.Parameters.AddWithValue("@WordLength", userInputInt);
+            cmd.Parameters.AddWithValue("@WordText", textGenerator(userInputInt));
+            cmd.ExecuteNonQuery();
+
+        }
+    }
+}
